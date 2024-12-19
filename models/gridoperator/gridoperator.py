ba4ddb91359926ef72de615eecaf2b066c388ec3
Valentin Seiler <valentin.seiler@fhv.at>
2024-12-19 17:24:36 +0100

    added smartmeter and gridoperator

--- models/gridoperator/gridoperator.py
+++ models/gridoperator/gridoperator.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
 class GridOperator():
-    def __init__(self, *smart_meter_models):
-        self.smart_meter_models = smart_meter_models
+    def __init__(self, name, delta_t=60*60*24, smart_meter_models=[]):
+        self.name = name
+        self.delta_t = delta_t
+        self.inputs = []
+        self.outputs = ['df_P_day']
+        self._smart_meter_models = smart_meter_models
 
-    def step(self, time, inputs):
-        for smart_meter_model in self.smart_meter_models:
-            self.smart_meter_data = smart_meter_model.retrieve_data()
+    def register_smartmeter(self, smart_meter_model):
+        self._smart_meter_models += [smart_meter_model]
+
+    def step(self, time):
+        pd.Series([]).rename()
+        df_P_day = pd.concat(
+            objs=[sm.retrieve_data() for sm in self._smart_meter_models], 
+            axis=1
+            )
+        return {'df_P_day': df_P_day}
