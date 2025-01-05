e9565c6559eca557c81f32867ea39640b2613448
Valentin Seiler <valentin.seiler@fhv.at>
2025-01-05 12:25:25 +0100

    updated smartmeter to new simplec multiinput feature

--- models/smart_meter/smart_meter.py
+++ models/smart_meter/smart_meter.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 class SmartMeter():
-    def __init__(self, name, n_loads=1):
+    def __init__(self, name):
         self.name = name
         self.delta_t = 60  # s
 
-        self.inputs = [f'P_{n}' for n in range(n_loads)]
+        self.inputs = ['P_'] # P_ as list attribute (endswith '_')
         self.outputs = ['P_grid']
 
         self._P_quarter_hourly_data = pd.Series([], name=self.name)
 
-    def step(self, time, **P_behind_meter):
-        P_grid = sum(P_behind_meter.values())
+    def step(self, time, P_):
+        P_grid = sum(P_)
         self._P_quarter_hourly_data[time] = P_grid
 
         return {'P_grid': P_grid}
--- models/smart_meter/test_smart_meter.py
+++ models/smart_meter/test_smart_meter.py
@@ -6,29 +6,29 @@ from models.smart_meter.smart_meter import SmartMeter
 
 
 def test_smartmeter_initialization():
-    smartmeter = SmartMeter('name', 2)
+    smartmeter = SmartMeter('name')
     assert smartmeter.name == 'name'
-    assert smartmeter.inputs == ['P_0', 'P_1']
+    assert smartmeter.inputs == ['P_']
     assert smartmeter.outputs == ['P_grid']
     assert smartmeter.delta_t == 60
     assert isinstance(smartmeter._P_quarter_hourly_data, pd.Series)
     assert smartmeter._P_quarter_hourly_data.name == 'name'
 
 def test_smartmeter_step():
-    smartmeter = SmartMeter('name', 1)
-    inputs = {'P_0':2., 'P_1': 3.}
+    smartmeter = SmartMeter('name')
+    inputs = {'P_': [2., 3.]}
     assert smartmeter.step(1, **inputs)['P_grid'] == 5.
 
 def test_smart_meter_retrieve_data_empty():
-    smartmeter = SmartMeter('name', 1)
+    smartmeter = SmartMeter('name')
     assert smartmeter.retrieve_data().empty, 'should return empty series'
 
 def test_smart_meter_retrieve_data():
-    smartmeter = SmartMeter('name', 1)
+    smartmeter = SmartMeter('name')
     P_values = np.random.random((30, 2))*2000 # random P values
 
     for i, (p1, p2) in enumerate(P_values): # Step model 30 times 
-        inputs = {'P_0':p1, 'P_1': p2}
+        inputs = {'P_': [p1, p2]}
         timestamp = pd.to_datetime(i, origin='2022-01-01 00:00', unit='m') # timestamp required, minute steps!
         smartmeter.step(timestamp, **inputs)
 
@@ -45,7 +45,7 @@ def test_smart_meter_retrieve_data():
     # repeat test after emptying dataframe
     P_values = np.random.random((30, 2))*2000 # new random P values
     for i, (p1, p2) in enumerate(P_values): # Step model 30 times again with new values
-        inputs = {'P_0':p1, 'P_1': p2}
+        inputs = {'P_': [p1, p2]}
         timestamp = pd.to_datetime(i+30, origin='2022-01-01 00:00', unit='m') # timestamp required, minute steps!
         smartmeter.step(timestamp, **inputs)
 
