0a0b2f7fa854fe8b229d891127b3d12c28fb0fc3
Valentin Seiler <valentin.seiler@fhv.at>
2025-01-29 08:01:38 +0000

    adaptations for linux server

--- models/pv/pv.py
+++ models/pv/pv.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from pathlib import Path
 
 class SynproPV():
     def __init__(self, name, P_pv_peak=50000):
         self.name = name
         self.delta_t = 60  # s
 
-        self.df = pd.read_csv(r'data\synPro\synPRO_Htg_H_3_1_App_9_Oc_18_MFHkl_MFH_5_Por_1_htg_17033.dat', header=45, sep=';', index_col=2).drop(['YYYYMMDD', 'hhmmss'], axis=1)
+        self.df = pd.read_csv(Path(r'data/synPro/synPRO_Htg_H_3_1_App_9_Oc_18_MFHkl_MFH_5_Por_1_htg_17033.dat'), header=45, sep=';', index_col=2).drop(['YYYYMMDD', 'hhmmss'], axis=1)
         self.df.index = pd.to_datetime(self.df.index, unit='s').shift(periods=+4, freq=pd.DateOffset(years = 1)).tz_localize('utc').tz_convert('Europe/Berlin') # shift to 2021, set timezone
         
         self.df = (self.df.loc[:, ['P_pvn']]*(-P_pv_peak)).rename({'P_pvn': 'P_pv'}, axis=1)
