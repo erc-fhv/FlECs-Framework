663d0dda29a218f37bfbec973c4d0c0f3e38aa2d
Valentin Seiler <valentin.seiler@fhv.at>
2025-01-30 08:38:59 +0000

    implemented grid operator model callback for controller

--- models/gridoperator/test_gridoperator.py
+++ models/gridoperator/test_gridoperator.py
@@ -6,13 +6,23 @@ from models.gridoperator.gridoperator import GridOperator
 
 
 class SmartMeterMock():
-    def __init__(self, name):
+    def __init__(self, name, mul, start_dt="2020-01-01 00:00"):
         self.name = name
+        date_range = pd.date_range(start=start_dt, end="2020-01-02 00:00", freq="15min")
+        values = np.arange(len(date_range))*mul
+        self.values = pd.Series(data=values, index=date_range, name=self.name)
 
     def retrieve_data(self):
-        date_range = pd.date_range(start="2020-01-01 00:00", end="2020-01-02 00:00", freq="15min")
-        random_values = np.random.randint(0, 2001, size=len(date_range))
-        return pd.Series(data=random_values, index=date_range, name=self.name)
+        return self.values
+    
+class ECMock():
+    def __init__(self):
+        self.name = 'ec'
+        self.df_P_day = pd.DataFrame([])
+   
+    def set_data(self, df_P_day):
+        self.df_P_day = df_P_day
+        
 
 def test_gridoperator_initialization():
     gridoperator = GridOperator('name')
@@ -23,19 +33,64 @@ def test_gridoperator_initialization():
 
 def test_gridoperator_register_smartmeter():
     gridoperator = GridOperator('name')
-    smartmeter = SmartMeterMock('sm1')
-    smartmeter1 = SmartMeterMock('sm2')
+    smartmeter = SmartMeterMock('sm1', 10)
+    smartmeter1 = SmartMeterMock('sm2', 10)
     gridoperator.register_smartmeter(smartmeter)
     gridoperator.register_smartmeter(smartmeter1)
 
     assert isinstance(gridoperator._smart_meter_models[0], SmartMeterMock)
     assert isinstance(gridoperator._smart_meter_models[1], SmartMeterMock)
 
-# def test_gridoperator_step():
-#     gridoperator = GridOperator('name')
-#     smartmeter = SmartMeterMock('sm1')
-#     smartmeter1 = SmartMeterMock('sm2')
-#     gridoperator.register_smartmeter(smartmeter)
-#     gridoperator.register_smartmeter(smartmeter1)
+def test_gridoperator_step_w_smartmeter():
+    gridoperator = GridOperator('name')
+    smartmeter = SmartMeterMock('sm1', 10)
+    smartmeter1 = SmartMeterMock('sm2', 20)
+    gridoperator.register_smartmeter(smartmeter)
+    gridoperator.register_smartmeter(smartmeter1)
 
-#     # print(gridoperator.step(1))
+    outputs = gridoperator.step(1)
+    assert outputs['df_P_day'].iloc[0, 0] == 0
+    assert outputs['df_P_day'].iloc[1, 0] == 10
+    assert outputs['df_P_day'].iloc[1, 1] == 20
+    assert outputs['df_P_day'].index[0] == pd.to_datetime("2020-01-01 00:00")
+
+def test_gridoperator_step_w_ec():
+    gridoperator = GridOperator('name')
+    smartmeter = SmartMeterMock('sm1', 10)
+    smartmeter1 = SmartMeterMock('sm2', 20)
+    gridoperator.register_smartmeter(smartmeter)
+    gridoperator.register_smartmeter(smartmeter1)
+
+    ec = ECMock()
+    gridoperator.register_callback_new_data(ec.set_data)
+
+    outputs = gridoperator.step(1)
+
+    assert outputs['df_P_day'].equals(ec.df_P_day)
+
+def test_gridoperator_multi_step():
+    gridoperator = GridOperator('name')
+    smartmeter = SmartMeterMock('sm1', 10)
+    smartmeter1 = SmartMeterMock('sm2', 20)
+    gridoperator.register_smartmeter(smartmeter)
+    gridoperator.register_smartmeter(smartmeter1)
+
+    ec = ECMock()
+    gridoperator.register_callback_new_data(ec.set_data)
+
+    outputs = gridoperator.step(1)
+    outputs = gridoperator.step(2)
+    outputs = gridoperator.step(2)
+
+    assert outputs['df_P_day'].equals(ec.df_P_day)
+
+def test_gridoperator_faulty_data():
+    gridoperator = GridOperator('name')
+    smartmeter = SmartMeterMock('sm1', 10, start_dt="2020-01-01 12:00")
+    smartmeter1 = SmartMeterMock('sm2', 20)
+    gridoperator.register_smartmeter(smartmeter)
+    gridoperator.register_smartmeter(smartmeter1)
+
+    outputs = gridoperator.step(1)
+    assert np.isnan(outputs['df_P_day'].iloc[0, 0]) 
+    assert np.isnan(outputs['df_P_day'].loc["2020-01-01 11:45", 'sm1']) 
