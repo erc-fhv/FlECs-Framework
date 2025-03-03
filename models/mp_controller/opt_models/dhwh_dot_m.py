190f279731576954a164ea74a9163fc6cd10b711
fionafeurstein <refi@fhv.at>
2025-03-03 08:53:38 +0100

    controller DHWH

--- models/mp_controller/opt_models/dhwh_dot_m.py
+++ models/mp_controller/opt_models/dhwh_dot_m.py
@@ -1,7 +1,9 @@
 import pyomo.environ as pyo
+from pyomo.environ import value
+import numpy as np
 
 class DHW_MILP_model_Temp(): 
-    def __init__(self, name, P_nom=2_000, C_tes=4200*100, T_tes_min=12, T_tes_max=80, eta=1, T_out=50, T_in=12, c_p=4200):
+    def __init__(self, name, P_nom=2_000, C_tes=4200*100, T_tes_min=10, T_tes_max=80, eta=1, T_out=50, T_in=12, c_p=4200):
         '''
         A domestic hot water heater with buffer storage MILP Model.
         The parent pyomo model is required to have the following attributes:
@@ -43,9 +45,14 @@ class DHW_MILP_model_Temp():
         # parameters that change for every run
         block.T_tes_0      = pyo.Param(mutable=True, domain=pyo.NonNegativeReals, validate=lambda _, E: self.T_tes_min <= E and E <= self.T_tes_max) # J
         block.dot_m_demand = pyo.Param(model.periods, mutable=True, domain=pyo.Reals) # Consumed Energy from storage in W (consumption < 0 < heat gain)
+        block.dot_m_demand_h = pyo.Var(model.periods, domain=pyo.Reals) # Consumed Energy from storage in W (consumption < 0 < heat gain)
+        block.slack_var     = pyo.Var(model.periods, domain=pyo.NonNegativeReals)
+        #block.slack_var_up     = pyo.Var(model.periods, domain=pyo.NonNegativeReals)
+        block.dot_m_on     = pyo.Var(model.periods, domain=pyo.Reals, bounds=(0,1))
 
         # Variables
         block.T_tes     = pyo.Var(model.timepoints, domain=pyo.NonNegativeReals, bounds=(self.T_tes_min, self.T_tes_max)) # J
+        #block.T_tes     = pyo.Var(model.timepoints, domain=pyo.NonNegativeReals, bounds=(0, self.T_tes_max)) # J
         block.on        = pyo.Var(model.periods, domain=pyo.Boolean) # 0/1
         block.P_el      = pyo.Var(model.periods, domain=pyo.NonNegativeReals) # W
 
@@ -55,11 +62,42 @@ class DHW_MILP_model_Temp():
 
         @block.Constraint(model.periods)
         def dhwh_tes_constraint_energy_balance(block, p):
-            return block.T_tes[p+1] == block.T_tes[p] + (block.P_el[p] * self.cop - (block.dot_m_demand[p] * self.c_p * (self.T_out - self.T_in))) * model.delta_t / self.C_tes
+            return block.T_tes[p+1] == block.T_tes[p] + (self.P_nom * block.on[p] * self.cop - (block.dot_m_demand[p] * self.c_p * (block.T_tes[p] - self.T_in))) * model.delta_t / self.C_tes
         
+        @block.Constraint(model.periods)
+        def dhwh_tes_constraint(block, p):
+            return block.dot_m_demand_h[p]  == block.dot_m_demand[p] * block.dot_m_on[p]
+
+        @block.Constraint(model.periods)
+        def dhwh_tes_constraint__(block, p):
+            return block.dot_m_demand_h[p]  == block.dot_m_demand[p] 
+
+        # @block.Expression(model.periods)
+        # def dhwh_tes_constraint__(block, p):
+        #     # if :
+        #     block.dot_m_on[p]  == 0
+        #     if (value(block.dot_m_demand[p]) > 0):
+        #         block.dot_m_on[p]  == 1
+        #     return block.dot_m_on[p]
+        #     # else:
+        #     #     return block.dot_m_on[p]  == 0
+
+        # @block.Constraint(model.periods)
+        # def dhwh_tes_constraint_(block, p):
+        #     #if p > 1:
+        #     return block.T_tes[p] <= self.T_tes_max + block.slack_var_up[p]
+        
+
+        @block.Constraint(model.periods)
+        def dhwh_tes_constraint_(block, p):
+            #if p > 1:
+            return block.T_tes[p] >= 35 * block.dot_m_on[p]- block.slack_var[p]
+            #else:
+            #    return pyo.Constraint.Skip
+ 
         block.tes_initial_condition = pyo.Constraint(rule=lambda block: block.T_tes[0] == block.T_tes_0)
 
-        block.nominal_power         = pyo.Constraint(model.periods, rule=lambda block, p: block.P_el[p] == self.P_nom * block.on[p])
+        block.nominal_power         = pyo.Constraint(model.periods, rule=lambda block, p: block.P_el[p] == self.P_nom * block.on[p] + block.slack_var[p]*1000)#+block.slack_var_up[p]*100000000000000)
 
 
 class DHW_MILP_model(): 
